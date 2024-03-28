8e7905b094124e8bbebddd58f6235e25f084f063
mhmd-zbib <mohammad.h.zbib@gmail.com>
2024-03-28 22:12:05 +0000

    fix: order service out of stock exception edit

--- order-service/src/main/java/dev/zbib/orderservice/exceptions/ErrorResponse.java
+++ order-service/src/main/java/dev/zbib/orderservice/exceptions/ErrorResponse.java
@@ -1,27 +1,22 @@
 package dev.zbib.orderservice.exceptions;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @SuperBuilder
-public class ErrorResponse extends RuntimeException {
+public class ErrorResponse {
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private String message;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private LocalDateTime timestamp;
 
     private String errorCode;
 
     private String status;
-
 
 }
