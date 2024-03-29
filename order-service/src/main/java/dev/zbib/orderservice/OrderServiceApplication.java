942f758dd67038e358f2457f9df46a01d59ce073
mhmd-zbib <mohammad.h.zbib@gmail.com>
2024-03-29 22:56:17 +0000

    feat: connected services to discovery server

--- order-service/src/main/java/dev/zbib/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/dev/zbib/orderservice/OrderServiceApplication.java
@@ -3,9 +3,11 @@ package dev.zbib.orderservice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @Slf4j
+@EnableDiscoveryClient
 public class OrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
